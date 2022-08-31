51ead571892aa6d7f26db2dec9a5dc47111312b1
Jeff McDowell <jeffreymc@gmail.com>
2022-08-31 17:34:36 -0400

    update to latest strapi version

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,10 @@
 module.exports = ({ env }) => ({
+  
+  'users-permissions': {
+    config: {
+    jwtSecret: env('JWT_SECRET')
+  },
+  },
   upload: {
     config: {
       provider: "cloudinary",
